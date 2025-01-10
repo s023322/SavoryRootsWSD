977912c5cc78c5c7830c74e145c05abc2d83a1ad
Matthew <107056920+s023322@users.noreply.github.com>
2025-01-09 20:51:12 -0500

    Add files via upload

--- src/navbar.js
+++ src/navbar.js
@@ -5,6 +5,7 @@ var highlight = document.getElementById("highlight");
 var navButtons = Array.from(document.getElementsByClassName("nav-button"));
 var main = document.getElementById("mainH");
 var small = document.getElementById("smallH");
+var currentPage = "";
 
 highlight.classList.remove("animated");
 
@@ -12,7 +13,7 @@ if (page[1]) {
     main.setAttribute("points", `0,0 0,0 0,0 0,0`);
     small.setAttribute("points", `0,0 ,0,0 0,0 0,0`);
     navButtons.forEach(element => {
-        if (page[1].replace("index", "home").includes(element.innerHTML.toLowerCase())) {
+        if (page[1].replace("index", "home").replace("documentation", "reference page").includes(element.innerHTML.toLowerCase())) {
             var hW = element.getBoundingClientRect().width;
             main.setAttribute("points", `2,5 ${8 + hW},5 ${6 + hW},19 0,19`);
             small.setAttribute("points", `${4 + hW},5 ${8 + hW},5 ${6 + hW},19 ${2 + hW},19`);
@@ -36,7 +37,22 @@ setTimeout(function () {
             small.setAttribute("points", `${4 + hW},5 ${8 + hW},5 ${6 + hW},19 ${2 + hW},19`);
             highlight.style.left = `${element.getBoundingClientRect().left - 6}px`;
             element.removeAttribute("href");
+            currentPage = pageName;
         }
-        console.log(element.href);
     });
 }, 1);
+
+console.log(currentPage)
+
+window.addEventListener("scroll", function () {
+    if (this.window.scrollY > 2) {
+        if (pageName.toLowerCase().includes("documentation")) {
+            main.style.opacity = "0";
+            small.style.opacity = "0";
+        }
+    }
+    else {
+        main.style.opacity = "0.5";
+        small.style.opacity = "0.5";
+    }
+})
